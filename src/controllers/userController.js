dcd21e2e4f611e9d1f17c2a233397dde9e2ab567
Sagar <sagarmaan2015@gmail.com>
2023-05-19 14:40:47 +0530

    login user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,8 +2,13 @@ const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { isValidObjectId } = require("mongoose");
+const { validateUserName , validateEmailId , validatePassword , ValidateRoleStatus } = require("../validations/validator")
+
+
 
 //================================= Register User ================================================//
+
+
 
 let userRegistration = async function (req, res) {
   try {
@@ -23,6 +28,12 @@ let userRegistration = async function (req, res) {
           "Please provide user name with suitable datatype OR it can't be empty",
       });
 
+      if (!validateUserName(userName)) {
+        return res
+          .status(400)
+          .send({ status: false, message: "User name should contain only alphabates." });
+      }
+
     if (!emailId || typeof emailId != "string")
       return res
         .status(400)
@@ -31,15 +42,11 @@ let userRegistration = async function (req, res) {
           messsage: "Email is mandatory with suitable datatype",
         });
 
-    // let checkEmailId = await userModel.findOne({ emailId : emailId });
-
-    // if (!checkEmailId)
-    //   return res
-    //     .status(404)
-    //     .send({
-    //       status: false,
-    //       message: "This emailId is not present in your database.",
-    //     });
+        if (!validateEmailId(emailId)) {
+          return res
+            .status(400)
+            .send({ status: false, message: "EmailId should be in a valid format." });
+        }
 
     if (!password || typeof password != "string")
       return res
@@ -49,6 +56,12 @@ let userRegistration = async function (req, res) {
           messsage: "Paasword is mandatory with suitable datatype",
         });
 
+        if (!validatePassword(password)) {
+          return res
+            .status(400)
+            .send({ status: false, message: "Password should be in a valid format." });
+        }
+
     let hashing = bcrypt.hashSync(password, 8);
     data.password = hashing;
 
@@ -56,6 +69,12 @@ let userRegistration = async function (req, res) {
       return res
         .status(400)
         .send({ status: false, message: "Role must be in string" });
+    }
+
+    if (!ValidateRoleStatus(role)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Role status should be Admin , Task Creator and Visitor ." });
     }
 
     if (!["Task Creator", "Admin", "Visitor"].includes(role.trim())) {
@@ -98,4 +117,77 @@ let userRegistration = async function (req, res) {
   }
 };
 
-module.exports = { userRegistration };
+
+//==================================== Login User ============================================//
+
+
+const userLogin = async function (req, res) {
+  try {
+    let { emailId, password } = req.body;
+
+    if (Object.keys(req.body).length == 0) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please input user Details" });
+    }
+    if (!emailId || typeof emailId != "string")
+    return res
+      .status(400)
+      .send({
+        status: false,
+        messsage: "Email is mandatory with suitable datatype",
+      });
+
+ 
+    if (!validateEmailId(emailId)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "EmailId should be Valid" });
+    }
+
+    if (!password || typeof password != "string")
+    return res
+      .status(400)
+      .send({
+        status: false,
+        messsage: "Paasword is mandatory with suitable datatype",
+      });
+
+
+      if (!validatePassword(password)) {
+        return res
+          .status(400)
+          .send({ status: false, message: "Password should be in a valid format." });
+      }
+
+    let verifyUser = await userModel.findOne({ emailId: emailId });
+    if (!verifyUser) {
+      return res
+      .status(400)
+      .send({ status: false, message: "User not found" });
+    }
+
+    let hash = verifyUser.password;
+
+    let isCorrect = bcrypt.compareSync(password, hash);
+    if (!isCorrect)
+      return res
+        .status(400)
+        .send({ status: false, message: "Password is incorrect" });
+
+    let payload = { userId: verifyUser["_id"], iat: Date.now() };
+    let jwtToken = jwt.sign(payload, "secret-key", { expiresIn: "1h" });
+
+    res.setHeader("jwtKey", jwtToken );
+    return res.status(200).send({
+      status: true,
+      message: "User login successfull",
+      data: { userId: verifyUser["_id"], jwtToken },
+    });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message });
+  }
+};
+
+
+module.exports = { userRegistration, userLogin, updateUser };

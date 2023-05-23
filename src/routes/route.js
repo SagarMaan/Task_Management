b1d665ffa2638b97cae084a0b49f2c6f5ccd519a
Sagar <sagarmaan2015@gmail.com>
2023-05-23 10:01:26 +0530

    routes file

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,26 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const {isAuthenticated , isAuthorized } = require("../middleware/commonMiddleware")
+const { userRegistration, userLogin, updateUser } = require("../controllers/userController");
+const { createTask , getTask , getTaskById , updateTask} = require("../controllers/taskController")
 
-const { userRegistration } = require("../controllers/userController");
+//======================================= User APIs =============================================//
 
-// ===================================== User API's ====================================== //
+router.post("/register", userRegistration );
+router.post("/login", userLogin );
+router.put("/update/:userId",isAuthenticated , updateUser);
 
+//===================================== Task APIs ===============================================//
 
-router.post( "/register" , userRegistration );
-
+router.post("/createTask/:userId", isAuthenticated, isAuthorized, createTask);
+router.get("/tasks/:userId", isAuthenticated, isAuthorized,getTask);
+router.get("/GetTask/:userId", isAuthenticated, isAuthorized , getTaskById);
+router.put("/UpdateTask/:userId", isAuthenticated, isAuthorized, updateTask);
 
 // ===================================== Invalid path ====================================== //
 
-
-router.all('/*', ( req , res ) => {
-    res.status(400).send({ status: false, message: " Path invalid." });
+router.all("/*", (req, res) => {
+  res.status(400).send({ status: false, message: " Path invalid." });
 });
 
 
-module.exports=router
+module.exports = router;

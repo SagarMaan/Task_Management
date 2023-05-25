320433826415e8040eddb2da9651d1d45c4d898a
Sagar <sagarmaan2015@gmail.com>
2023-05-25 10:04:06 +0530

    small changes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+const route = require("./routes/route");
+const mongoose = require("mongoose");
 
-const express = require('express')
-require('dotenv').config();
-const route = require('./routes/route')
-const mongoose = require('mongoose')
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-app.use(express.json())
-
-
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
-})
-    .then(() => {console.log("MongoDB is connected")})
-    .catch((err) => {console.log(err.message)})
+  })
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-app.use("/", route)
+app.use("/", route);
 
 app.listen(process.env.PORT, function () {
-    console.log(`Express app running on ${process.env.PORT} `)
-})    
+  console.log(`Express app running on ${process.env.PORT} `);
+});
--- src/validations/validator.js
+++ src/validations/validator.js
@@ -1,35 +1,50 @@
 //======================================= Name Regex Validation ========================================//
+const validateUserName = (userName) => {
+  userName = userName.trim()
+  return /^(?=.{1,50}$)[a-z]+(?:['_.\s][a-z]+)*$/i.test(userName);
+};
 
+//====================================== Email Regex Validation =======================================//
+const validateEmailId = (emailId) => {
+  emailId = emailId.trim()
+  return /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/.test(
+    emailId
+  );
+};
 
-const validateUserName = (name) => {
-    return (/^(?=.{1,50}$)[a-z]+(?:['_.\s][a-z]+)*$/i.test(name))
-  }
-  
-  
-  
-  //====================================== Email Regex Validation =======================================//
-  
-  
-  const validateEmailId = (emailId) => {
-    return (/^[a-z]{1}[a-z0-9._]{1,100}[@]{1}[a-z]{2,15}[.]{1}[a-z]{2,10}$/.test(emailId))
-  }
-  
-  
-  //===================================== Password Regex Validation ====================================//
-  
-  
-  const validatePassword = (password) => {
-    return (/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/.test(password));
-  }
+//===================================== Password Regex Validation ====================================//
+const validatePassword = (password) => {
+  password = password.trim()
+  return /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/.test(
+    password
+  );
+};
 
-  
-  // ===================================== Role validation ====================================== //
-  
-  
-  const ValidateRoleStatus = (role) => { 
-    return (["Task Creator", "Admin", "Visitor"].indexOf(role)) !== -1 
-  }
-  
-  
-  module.exports = { validateUserName , validateEmailId , validatePassword , ValidateRoleStatus }
-  
+// ===================================== Status validation ====================================== //
+
+const validateRoleStatus = (role) => {
+  role = role.trim()
+  return ["Task Creator", "Admin", "Visitor"].indexOf(role) !== -1;
+};
+
+// ===================================== Status validation ====================================== //
+
+const validateTaskStatus = (status) => {
+  status = status.trim();
+  return ["ToDo", "In_Progress", "Completed"].indexOf(status) !== -1;
+};
+// ===================================== Status validation ====================================== //
+
+const validateTaskPriority = (priority) => {
+  priority = priority.trim()
+  return ["low", "medium", "high"].indexOf(priority) !== -1;
+};
+
+module.exports = {
+  validateUserName,
+  validateEmailId,
+  validatePassword,
+  validateRoleStatus,
+  validateTaskStatus,
+  validateTaskPriority
+};
